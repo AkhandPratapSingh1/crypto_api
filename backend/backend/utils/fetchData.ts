2ed3133194f42340eb01b29422bba296041ab188
Akhand Pratap Singh <akhandpratap6397@gmail.com>
2024-07-23 19:35:44 +0530

    remove env file

--- backend/backend/utils/fetchData.ts
+++ backend/backend/utils/fetchData.ts
@@ -10,7 +10,7 @@ interface PriceResponse {
     usd_market_cap: number;
     usd_24h_vol: number;
     usd_24h_change: number;
-    last_updated_at: number; // Timestamp in seconds
+    last_updated_at: number;
   };
 }
 
